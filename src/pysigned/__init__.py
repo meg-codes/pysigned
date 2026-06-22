a609eafddca382cfde045a9b5c5526363780ca82
Meg Hicks <17815297+meg-codes@users.noreply.github.com>
2026-06-22 00:05:10 -0400

    bring test coverage up to 100

--- src/pysigned/__init__.py
+++ src/pysigned/__init__.py
@@ -1,29 +1,23 @@
 from .backends import (
-    Backend,
     Ed25519Backend,
     Ed25519KeySet,
-    HMACBackend,
     HMACKeySet,
-    KeySet,
 )
+
 from .keys import (
     Ed25519PrivateKey,
     Ed25519PublicKey,
     HMACKey,
-    Key,
 )
+
 from .signature import URLAuth
 
 __all__ = [
-    "Backend",
     "Ed25519Backend",
     "Ed25519KeySet",
     "Ed25519PrivateKey",
     "Ed25519PublicKey",
-    "HMACBackend",
     "HMACKey",
     "HMACKeySet",
-    "Key",
-    "KeySet",
     "URLAuth",
 ]
